f960626edb5847449a6db6eebd6ed3af07b7aaac
Vlad <157218536+VladKrasny@users.noreply.github.com>
2024-02-04 22:38:03 +0100

    import the remote app to the host app

--- apps/host/src/TheApp/TheApp.tsx
+++ apps/host/src/TheApp/TheApp.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { TheButton } from "@mfe/shared-ui";
+import TheRemoteApp from "remoteApp/TheApp";
 
 export function TheApp() {
   const [count, setCount] = useState(0);
@@ -13,6 +14,8 @@ export function TheApp() {
       <h1>Host</h1>
 
       <TheButton onClick={increment}>Host count is {count}</TheButton>
+
+      <TheRemoteApp />
     </>
   );
 }
--- apps/host/vite.config.ts
+++ apps/host/vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    port: 5001,
+  },
+  preview: {
+    port: 6001,
+  },
+  build: {
+    target: "esnext",
+  },
+  plugins: [
+    react(),
+    federation({
+      name: "hostApp",
+      remotes: {
+        remoteApp: "http://localhost:6002/assets/remoteEntry.js",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
 });
